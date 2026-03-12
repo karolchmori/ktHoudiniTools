8fc26c2a93a18c9570f73f93a7687a21b62a005e
karolchmori <72026498+karolchmori@users.noreply.github.com>
2026-03-12 23:28:58 +0100

    MOD - Groom link nodes of mask

--- kt_GroomingKit.py
+++ kt_GroomingKit.py
@@ -1,5 +1,6 @@
 
 import hou
+import toolutils
 
 def fooText():
     print("This is a text")
@@ -8,15 +9,14 @@ def fooText():
 
 def createMasks(kwargs):
     hda = kwargs['node']
-    root = hda.node('HDA_MASKS')
+    root = hda.node('HDA_IN_GEO/HDA_MASKS')
     startPositionA = root.node('BEFORE_MASK_BODY')
     startPositionB = root.node('BEFORE_MASK_SUBD')
     mergeNodeA = root.node('merge_MASKS_BODY')
     mergeNodeB = root.node('merge_MASKS_SUBD')
     amountInstances = hda.parm('maskList').eval()
 
-    amountA = 0
-    amountB = 0
+    
 
     # 1. Clean up old nodes before creating new ones
     for child in root.children():
@@ -24,57 +24,88 @@ def createMasks(kwargs):
             child.destroy()
 
     # 2. Clean up connections between BEFORE_GROUPS and merge_GROUPS
-    for i in range(1, len(mergeNodeA.inputs())):
-        mergeNodeA.setInput(i, None)
+    for m in [mergeNodeA, mergeNodeB]:
+        for i in range(0, len(m.inputs())):
+            m.setInput(i, None)
 
-    
-    for i in range(1, len(mergeNodeB.inputs())):
-        mergeNodeB.setInput(i, None)
+    #for i in range(0, len(mergeNodeA.inputs())):
+        
+    #    mergeNodeA.setInput(i, None)
 
 
+    # 2. Always connect the base geometry to the first input (Index 0)
+    #mergeNodeA.setInput(0, startPositionA)
+    #mergeNodeB.setInput(0, startPositionB)
 
-    # 2. Depending on the amount of Instances we'll clean up or recreate
-    if amountInstances == 0:
-        # Reconnect BEFORE_GROUPS directly to merge_GROUPS
+    # Counters for subsequent inputs (starting at 1)
+    countA = 0
+    countB = 0
+
+    # 3. Create and wire new nodes
+    for i in range(1, amountInstances + 1):
+        name = f'HDA_mask{i}'
+
+        # Check if the node exists
+        maskNode = root.node(name)
+        
+        # If it doesn't exist then create it and wire it
+        if not maskNode:
+            maskNode = root.createNode('attribpaint', name)
+        
+        # Wire Input: Connect to the 'BEFORE_GROUPS' null
+        choice = hda.parm(f'maskBase_{i}').eval()
+
+        if choice == 0:
+            maskNode.setInput(0, startPositionA)
+            mergeNodeA.setInput(countA, maskNode)
+            countA += 1
+        else:
+            maskNode.setInput(0, startPositionB)
+            mergeNodeB.setInput(countB, maskNode)
+            countB += 1
+
+        
+        # Relink data
+        maskNode.parm('attribname1').setExpression(f'chs("../../../maskName_{i}")')
+        maskNode.parm('attribtype1').setExpression(f'ch("../../../maskType_{i}")')
+        maskNode.parm('attribreset1').setExpression(f'ch("../../../maskReset_{i}")')
+        maskNode.parm('stroke_shape').setExpression(f'ch("../../../maskStrokeShape_{i}")')
+        maskNode.parm('fgfloat').setExpression(f'ch("../../../maskFG_{i}")')
+        maskNode.parm('bgfloat').setExpression(f'ch("../../../maskBG_{i}")')
+        maskNode.parm('stroke_radius').setExpression(f'ch("../../../maskStrokeRadius_{i}")')
+        maskNode.parm('stroke_opacitypressure').setExpression(f'ch("../../../maskStrokeOpacityPressure_{i}")')
+        maskNode.parm('stroke_radiuspressure').setExpression(f'ch("../../../maskStrokeRadiusPressure_{i}")')
+
+        maskNode.parm('domirror').setExpression(f'ch("../../../maskMirror_{i}")')
+        maskNode.parm('mirror_tx').setExpression(f'ch("../../../maskMirrorOri_{i}x")')
+        maskNode.parm('mirror_ty').setExpression(f'ch("../../../maskMirrorOri_{i}y")')
+        maskNode.parm('mirror_tz').setExpression(f'ch("../../../maskMirrorOri_{i}z")')
+        maskNode.parm('mirror_dirx').setExpression(f'ch("../../../maskMirrorDir_{i}x")')
+        maskNode.parm('mirror_diry').setExpression(f'ch("../../../maskMirrorDir_{i}y")')
+        maskNode.parm('mirror_dirz').setExpression(f'ch("../../../maskMirrorDir_{i}z")')
+
+        maskNode.parm('recachemethod').setExpression(f'ch("../../../maskCacheMethod_{i}")')
+        maskNode.parm('recache').setExpression(f'ch("../../../maskCacheStrokes_{i}")')
+        maskNode.parm('savecache').setExpression(f'ch("../../../maskCacheSave_{i}")')
+        maskNode.parm('livemode').setExpression(f'ch("../../../maskCacheLiveMode_{i}")')
+        maskNode.parm('docaching').setExpression(f'ch("../../../maskCacheDo_{i}")')
+        maskNode.parm('erasestrokes').setExpression(f'ch("../../../maskStrokeHistoryClear_{i}")')
+
+        maskNode.parm('movestashtofile').setExpression(f'ch("../../../maskStashMoveFile_{i}")')
+        maskNode.parm('loadstashfromfile').setExpression(f'ch("../../../maskStashLoadFile_{i}")')
+        maskNode.parm('strokegeofile').setExpression(f'chs("../../../maskSnapStroke_{i}")')
+        maskNode.parm('bakedgeofile').setExpression(f'chs("../../../maskSnapBakedGeo_{i}")')
+  
+
+
+    if mergeNodeA.input(0) is None:
         mergeNodeA.setInput(0, startPositionA)
+        
+    if mergeNodeB.input(0) is None:
         mergeNodeB.setInput(0, startPositionB)
 
-    else:
+    
 
-        # 3. Create and wire new nodes
-        for i in range(1, amountInstances + 1):
-            name = f'HDA_mask{i}'
-
-            # Check if the node exists
-            maskNode = root.node(name)
-            
-            # If it doesn't exist then create it and wire it
-            if not maskNode:
-                maskNode = root.createNode('attribpaint', name)
-            
-            # Wire Input: Connect to the 'BEFORE_GROUPS' null
-            choice = hda.parm(f'maskBase_{i}').eval()
-
-            if choice == 0:
-                maskNode.setInput(0, startPositionA)
-                mergeNodeA.setInput(amountA, maskNode)
-                amountA = amountA + 1
-            else:
-                maskNode.setInput(0, startPositionB)
-                mergeNodeB.setInput(amountB, maskNode)
-                amountB = amountB + 1
-
-            
-            # Relink data
-            maskNode.parm(f'attribname1').setExpression(f'chs("../../maskName_{i}")')
-            maskNode.parm('stroke_shape').setExpression(f'chs("../../maskStrokeShape_{i}")')
-            maskNode.parm('fgfloat').setExpression(f'ch("../../maskFG_{i}")')
-            maskNode.parm('bgfloat').setExpression(f'chs("../../maskBG_{i}")')
-            maskNode.parm('stroke_radius').setExpression(f'ch("../../maskStrokeRadius_{i}")')
-            maskNode.parm('stroke_opacitypressure').setExpression(f'ch("../../maskStrokeOpacityPressure_{i}")')
-            maskNode.parm('stroke_radiuspressure').setExpression(f'ch("../../maskStrokeRadiusPressure_{i}")')
-            
-            
     root.layoutChildren()        
     
 
@@ -82,7 +113,9 @@ def createMasks(kwargs):
 
 def createGroups(kwargs):
     hda = kwargs['node']
-    root = hda.node('HDA_GEO')
+    root = hda.node('HDA_IN_GEO/HDA_GEO')
+
+    print(root)
 
     startPosition = root.node('BEFORE_GROUPS')
     mergeNode = root.node('merge_GROUPS')
@@ -122,14 +155,28 @@ def createGroups(kwargs):
             mergeNode.setInput(i-1, groupNode)
             
             # Relink data
-            groupNode.parm('groupname').setExpression(f'chs("../../groupName_{i}")')
-            groupNode.parm('grouptype').setExpression(f'ch("../../groupType_{i}")')
-            groupNode.parm('basegroup').setExpression(f'chs("../../groupBase_{i}")')
-            groupNode.parm('geotype').setExpression(f'ch("../../groupGeoType_{i}")')
-            groupNode.parm('ordered').setExpression(f'ch("../../groupGeoOrdered_{i}")')
+            groupNode.parm('groupname').setExpression(f'chs("../../../groupName_{i}")')
+            groupNode.parm('grouptype').setExpression(f'ch("../../../groupType_{i}")')
+            groupNode.parm('basegroup').setExpression(f'chs("../../../groupBase_{i}")')
+            groupNode.parm('geotype').setExpression(f'ch("../../../groupGeoType_{i}")')
+            groupNode.parm('ordered').setExpression(f'ch("../../../groupGeoOrdered_{i}")')
             
             
 
             #print(f'Group {i}')
         
     root.layoutChildren()
+
+
+
+def paintMask(kwargs):
+    hda = kwargs['node']
+    idx = kwargs.get('script_multiparm_index', 1)
+
+    paint_node = hda.node(f'HDA_IN_GEO/HDA_MASKS/HDA_mask{idx}')
+
+    
+    if paint_node:
+        # Force Houdini to focus on the internal paint node
+        paint_node.setSelected(True, clear_all_selected=True)
+        paint_node.setDisplayFlag(True)
